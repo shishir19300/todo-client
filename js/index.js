238bece93132ea91b6d14c62f20d11abd6cf55dd
shishir19300 <shishirlamsal2004@gmail.com>
2026-03-23 09:17:50 +0200

    updated the backend url

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001'
+const BACKEND_ROOT_URL = 'https://todo-server-7pcq.onrender.com'
 import { Todos } from "./class/Todos.js"
 
 const todos = new Todos(BACKEND_ROOT_URL)
